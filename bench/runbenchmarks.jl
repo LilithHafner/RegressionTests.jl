a777f1319aa9bbb0d4a8534c01f477b243418eba
Lilith Orion Hafner <lilithhafner@gmail.com>
2023-11-14 14:52:45 -0600

    clean up benchmarks.jl and make it's nominal runtime 2s (14.1s total nominal runtime with 15 processess) to investigate why the true runtime is 52.3s

--- bench/runbenchmarks.jl
+++ bench/runbenchmarks.jl
@@ -1,15 +1,10 @@
-# println("Hi!")
-# println(pwd())
-import Pkg
-# println(Pkg.status())
 using TablemarksCI
 using Chairmarks
-@b 1+1
-@be 1+1
-# 463.5 => 306.8
+
+# 463.5 => 306.8 => 52.3
 for n in 1:200
     @track begin
-        res = @be n rand
+        res = @be n rand seconds=.01
         [minimum(res).time, Chairmarks.median(res).time, Chairmarks.mean(res).time]
     end
     # TODO: use "group" syntax
@@ -21,6 +16,6 @@ for n in 1:200
     # end
 end
 
-for k in 1:1_000_00#0
-    @track k
-end
+# for k in 1:1_000_00#0
+#     @track k
+# end
