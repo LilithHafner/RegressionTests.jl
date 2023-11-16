4a64bb15adaac0f1b5153758991cbdaf566fcdf1
Lilith Orion Hafner <lilithhafner@gmail.com>
2023-11-15 22:53:16 -0600

    add more precise correctness tests (i.e. for which `@track`s changes are reported) and print reported changes during testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -24,7 +24,11 @@ using Pkg
                 old_src = read(src_file, String)
                 new_src = replace(old_src, "my_sum(x) = sum(x)" => "my_sum(x) = sum(Float64.(x))")
                 write(src_file, new_src)
-                @test !isempty(runbenchmarks(project = ".")) # Fail
+                changes = runbenchmarks(project = ".") # Fail
+                @test !isempty(changes)
+                @test !any(occursin("my_prod", c.expr) for c in changes) # Those didn't change
+                @test any(occursin("my_sum", c.expr) for c in changes) # This did change
+                println.(changes)
                 run(`git add $src_file`)
                 run(`git commit -m "Introduce regression"`)
                 @test isempty(runbenchmarks(project = ".")) # Pass
