e0a1465bd961be9aeb3b5c11325dd8c129524570
Lilith Orion Hafner <lilithhafner@gmail.com>
2023-11-18 15:28:20 -0600

    Make an effort to automatically fetch

--- src/RegressionTests.jl
+++ src/RegressionTests.jl
@@ -97,6 +97,16 @@ function runbenchmarks(;
         if rev == "dev"
             Pkg.develop(PackageSpec(path=project), io=devnull)
         else
+            cd(project) do # Mostly for CI
+                if success(`git status`) && !success(`git rev-parse --verify $rev`)
+                    iob = IOBuffer()
+                    wait(run(`git remote`, stdout=iob, wait=false))
+                    remotes = split(String(take!(iob)), '\n')
+                    if length(remotes) == 1
+                        run(`git fetch $(only(remotes)) $rev`) # Assume that the remote is origin :(
+                    end
+                end
+            end
             Pkg.add(PackageSpec(path=project, rev=rev), io=devnull)
         end
         Pkg.instantiate(io=devnull)
