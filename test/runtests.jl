e2b2e9bb996e38b199b9a4db0504dd3b066ef16a
Lilith Orion Hafner <lilithhafner@gmail.com>
2023-11-19 07:56:46 -0600

    debug

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,7 @@ rev = "main"
 
 run(`git branch`)
 
-# run(ignorestatus(`git fetch origin $rev`))
+run(ignorestatus(`git fetch --all $rev`))
 
 println(success(`git rev-parse --verify $rev`))
 run(ignorestatus(`git checkout $rev`))
