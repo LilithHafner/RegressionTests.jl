eaaa7dad795e87b69cfbc56d6eac8a8ed211b6ae
Lilith Orion Hafner <lilithhafner@gmail.com>
2023-11-21 16:12:30 -0600

    Support apple on 1.9 (#7)

--- src/RegressionTests.jl
+++ src/RegressionTests.jl
@@ -40,7 +40,7 @@ function report_changes(changes)
     end
 end
 
-is_platform_supported() = VERSION >= v"1.6" && !Sys.iswindows() && !(VERSION.minor == 9 && Sys.isapple())
+is_platform_supported() = VERSION >= v"1.6" && !Sys.iswindows()
 
 function test(::Type{Bool}; skip_unsupported_platforms=false)
     if skip_unsupported_platforms && !is_platform_supported()
