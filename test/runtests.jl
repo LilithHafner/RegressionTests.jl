d90908f830014a257150747efb5f359900a7c4ac
Lilith Orion Hafner <lilithhafner@gmail.com>
2024-03-21 08:20:23 -0500

    log runtime in CI

--- test/runtests.jl
+++ test/runtests.jl
@@ -48,14 +48,16 @@ using Pkg
                     old_src = read(src_file, String)
                     new_src = replace(old_src, "my_sum(x) = sum(x)" => "my_sum(x) = sum(Float64.(x))")
                     write(src_file, new_src)
-                    changes = runbenchmarks(project = ".") # Fail
+                    t = @elapsed changes = runbenchmarks(project = ".") # Fail
+                    println("Runtime for positive runbenchmarks: $t")
                     @test !isempty(changes)
                     @test !any(occursin("my_prod", c.expr) for c in changes) # Those didn't change [This is the a false positive test]
                     @test any(occursin("my_sum", c.expr) for c in changes) # This did change
                     println.(changes)
                     run(`git add $src_file`)
                     run(`git commit -m "Introduce regression"`)
-                    @test isempty(runbenchmarks(project = ".")) # Pass
+                    t = @time @test isempty(runbenchmarks(project = ".")) # Pass
+                    println("Runtime for negative runbenchmarks: $t")
                     # TODO: handle this case well
                     # TODO: track the runtime of these runbenchmark calls... but we can't use RegressionTests.jl because that would be too slow.
                 finally
