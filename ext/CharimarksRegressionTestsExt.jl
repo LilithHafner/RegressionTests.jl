f5acafc77196ff65e551c70f370f7f09f81a99eb
Lilith Orion Hafner <lilithhafner@gmail.com>
2024-03-25 14:16:05 -0500

    don't track evals, warmup, or checksum

--- ext/CharimarksRegressionTestsExt.jl
+++ ext/CharimarksRegressionTestsExt.jl
@@ -5,11 +5,13 @@ module CharimarksRegressionTestsExt
 
 using Chairmarks, RegressionTests
 
+const tracked_properties = (:time, :allocs, :bytes, :gc_fraction, :compile_fraction, :recompile_fraction)
+
 RegressionTests.trackable(s::Chairmarks.Sample) =
-    NamedTuple(name => getproperty(s, name) for name in propertynames(s))
+    NamedTuple(name => getproperty(s, name) for name in tracked_properties)
 
 RegressionTests.trackable(b::Chairmarks.Benchmark) = NamedTuple(Symbol(symbol, " ", name) =>
     getproperty(sam, name) for (symbol, sam) in ((s, getproperty(Chairmarks, s)(b)) for s in
-    (:minimum, :median, :mean)) for name in propertynames(sam))
+    (:minimum, :median, :mean)) for name in tracked_properties)
 
 end
